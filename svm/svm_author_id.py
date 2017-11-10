20bc2854b5edc39a6a7d604faa2fb3f3a3d793ff
Erik Swenson <erik.swens@gmail.com>
2017-11-10 13:24:50 -0700

    Complete SVM Author ID project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -27,4 +27,44 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+C_param = 10000;
+
+# Cut down the training data by 99%.
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+# Create SVM classifier and train it.
+print "Training SVM classifier with C =", C_param, ". . ."
+classifier = svm.SVC(C=10000, kernel="rbf")
+
+# Train the classifier.
+t0 = time()
+classifier.fit(features_train, labels_train)
+print "\tTraining time: ", round(time() - t0, 3), "s"
+
+# Make some predictions
+print "Making predictions . . ."
+t0 = time()
+pred = classifier.predict(features_test)
+print "\tPrediction time: ", round(time() - t0, 3), "s"
+
+# Calculate the accuracy of the SVM.
+print "Calculating the accuracy of the SVM . . ."
+score = accuracy_score(labels_test, pred, normalize=True)
+print "\tAccuracy ( C =", C_param, "): ", score
+
+print "Predictions:"
+print "\t#10 =", pred[10]
+print "\t#26 =", pred[26]
+print "\t#50 =", pred[50]
+
+# Count how many of the emails are classified to be authored by Chris (1)
+chris_count = 0
+for email in pred:
+    if email == 1:
+        chris_count += 1
+print "Number of Chris emails: ", chris_count
 
